2e0dffded5ff18c7faace51d67d69b6d28682c8d
Dundo <107737568+ganud@users.noreply.github.com>
2023-10-01 15:44:35 -0400

    Dummy content website set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './style.css';
+import clearContent from "./page-loader.js";
---
+++ src/page-loader.js
@@ -0,0 +1,4 @@
+export default function clearContent() {
+    let content = document.getElementsByClassName('card')[0];
+    content.innerHTML = "";
+}
